31e8cf9a320832379904ff126ac4a63a0d0e6a3b
eigengrund-space <>
2026-07-23 14:07:43 +0000

    Sync: Mein Raum: Abstände im Header vergrößert

--- templates/template-mein-raum.php
+++ templates/template-mein-raum.php
@@ -99,13 +99,13 @@ get_header(); ?>
 
 <div class="eg-mr-wrap">
 
-  <div class="eg-tag" style="margin-bottom:.75rem;">Mein Raum</div>
+  <div class="eg-tag" style="margin-bottom:.75rem;">Mein Raum</div> <br> <br>
 
   <h1 style="margin-top:0;margin-bottom:26px;">Willkommen, <em style="color:var(--eg-amber);"><?php echo do_shortcode('[pmpro_member field="first_name"]'); ?></em></h1>
 
   <div style="width:36px;height:.5px;background:var(--eg-amber);margin:0 0 1.5rem;"></div>
 
-  <div style="font-size:13px;color:var(--eg-muted);margin-bottom:2rem;">
+  <div style="font-size:13px;color:var(--eg-muted);margin-bottom:2rem;"> <br> <br>
     Mitglied seit <?php echo do_shortcode('[eg_mein_raum_seit]'); ?>
   </div>
 
